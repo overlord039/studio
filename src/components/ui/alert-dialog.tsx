eae80d65898d35bc97f21b65d731fc1e7c5504ea
Durga Sankar <gdsr1613405@gmail.com>
2025-07-04 17:51:25 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Primitive.div rol

--- src/components/ui/alert-dialog.tsx
+++ src/components/ui/alert-dialog.tsx
@@ -65,7 +65,7 @@ const AlertDialogFooter = ({
 }: React.HTMLAttributes<HTMLDivElement>) => (
   <div
     className={cn(
-      "flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2",
+      "flex flex-row justify-end space-x-2",
       className
     )}
     {...props}
@@ -118,7 +118,6 @@ const AlertDialogCancel = React.forwardRef<
     ref={ref}
     className={cn(
       buttonVariants({ variant: "outline" }),
-      "mt-2 sm:mt-0",
       className
     )}
     {...props}
