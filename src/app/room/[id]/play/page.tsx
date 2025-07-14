984f3ea02dbb37408d857ab584968b32fea8b16c
Durga Sankar <gdsr1613405@gmail.com>
2025-07-14 15:01:50 +0000

    1 or 2 Tickets: Displayed in a single column. 3 Tickets: Arranged with t

--- src/app/room/[id]/play/page.tsx
+++ src/app/room/[id]/play/page.tsx
@@ -1000,17 +1000,3 @@ export default function GameRoomPage() {
 function formatCurrency(amount: number) {
     return new Intl.NumberFormat('en-IN', { style: 'currency', currency: 'INR', minimumFractionDigits: 2, maximumFractionDigits: 2 }).format(amount);
 };
-
-    
-
-
-
-
-
-    
-
-
-
-
-
-
