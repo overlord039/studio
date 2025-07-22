c10a4d837b9591861eb264781afba9c3089892e5
Durga Sankar <gdsr1613405@gmail.com>
2025-07-22 11:07:32 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Primitive.div rol

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -26,7 +26,7 @@ const GoogleIcon = (props: React.SVGProps<SVGSVGElement>) => (
     </svg>
 );
 
-const AVATAR_IMAGES = Array.from({ length: 6 }, (_, i) => `/userimages/u${i + 1}.png`);
+const AVATAR_IMAGES = Array.from({ length: 7 }, (_, i) => `/userimages/u${i + 1}.png`);
 
 const AvatarSelectionDialog = ({ onSelect, children }: { onSelect: (src: string) => void; children: React.ReactNode }) => {
   const [open, setOpen] = useState(false);
@@ -259,4 +259,3 @@ export default function ProfilePage() {
     </div>
   );
 }
-
